65a80de38b2c6abbdf8f755726af9ec2d5de9099
Amar Nepali <amar@Amars-MacBook-Pro.local>
2024-09-19 23:12:23 +0930

    lets try to use gpu

--- main/script_new.sh
+++ main/script_new.sh
@@ -3,10 +3,10 @@
 #SBATCH --job-name=nepa0034orbOpt
 #SBATCH --output=orbOpt-log.txt
 #SBATCH --error=logs/%A.error
-#SBATCH --partition=melfu
+#SBATCH --partition=gpu
 #SBATCH --cpus-per-task=16
 #SBATCH --mem-per-cpu=16G
-#SBATCH --gres="gpu:0"
+#SBATCH --gres="gpu:1"
 #SBATCH --time=1-0
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=nepa0034@flinders.edu.au
