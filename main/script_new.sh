2b1a9ed93f76e1601b161b3ad20d1702d5f5d121
Amar Nepali <amar@Amars-MacBook-Pro.local>
2024-09-19 23:10:17 +0930

    new script update

--- main/script_new.sh
+++ main/script_new.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
-#SBATCH --job-name long_job_nsga3
-#SBATCH --qos=short
-#SBATCH --reservation=shortqos
+#SBATCH --ntasks=1 # indicate the number of core required
+#SBATCH --job-name=nepa0034orbOpt
+#SBATCH --output=orbOpt-log.txt
+#SBATCH --error=logs/%A.error
+#SBATCH --partition=melfu
+#SBATCH --cpus-per-task=16
+#SBATCH --mem-per-cpu=16G
+#SBATCH --gres="gpu:0"
+#SBATCH --time=1-0
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=nepa0034@flinders.edu.au
 
+####################
 
 source /home/nepa0034/.bashrc
 conda activate PyEnv39
