0419b6be49d27ff4652170002b0081308dec6599
Amar Nepali <amar@Amars-MacBook-Pro.local>
2024-09-27 21:08:59 +0930

    DHM_ILC and ILM_DHC

---
+++ main/script_ILM_DHC_50.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --ntasks=1 # indicate the number of core required
+#SBATCH --job-name=nepa0034_ILM_DHCC_50P
+#SBATCH --output=/home/$FAN/%x-%j.out.txt
+#SBATCH --error=/home/$FAN/%x-%j.err.txt
+#SBATCH --partition=gpu
+#SBATCH --cpus-per-task=16
+#SBATCH --mem-per-cpu=8G
+#SBATCH --gres="gpu:1"
+#SBATCH --time=5-0
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=nepa0034@flinders.edu.au
+
+####################
+
+source /home/nepa0034/.bashrc
+conda activate PyEnv39
+
+python3 optimization_ILM_DHC_50P_100G.py
